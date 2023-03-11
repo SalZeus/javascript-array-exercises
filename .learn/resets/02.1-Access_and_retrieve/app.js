c1d2452dc069963e33084c32fcc05dea95eaa359
SalZeus <juanfeguto2@gmail.com>
2023-03-11 00:13:17 +0000

    .

--- .learn/resets/02.1-Access_and_retrieve/app.js
+++ .learn/resets/02.1-Access_and_retrieve/app.js
@@ -2,8 +2,7 @@
 let myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
 
 //1. print the item here
-console.log(myArray.index(3));
+
 //2. change 'thursday'a value here to null
-myArray.index(4) = null;
+
 //3. print the position of step 2
-console.log(miArray.index(null));
